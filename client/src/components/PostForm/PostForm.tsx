65ef925426dd25fca6701c8ba1d5d4aba5eb92c7
Виктория Голубова <v.golubova@denexy.com>
2024-02-08 12:43:38 +0400

    Form validation.

--- client/src/components/PostForm/PostForm.tsx
+++ client/src/components/PostForm/PostForm.tsx
@@ -1,38 +1,42 @@
-import {FC, FormEventHandler, useState} from 'react';
+import {FC} from 'react';
 import { Button } from '../Button';
 import { FormField } from '../FormField';
 import './PostForm.css';
 import {useMutation} from "@tanstack/react-query";
 import {createPost} from "../../api/Post";
 import {queryClient} from "../../api/queryClient";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 export interface IPostFormProps {}
 
+const CreatePostSchema = z.object({
+    text: z.string().min(10, "Длина поста должна быть не менее 10 символов")
+});
+
+type CreatePostForm = z.infer<typeof CreatePostSchema>;
+
 export const PostForm: FC<IPostFormProps> = () => {
-  const [text, setText] = useState("");
+  const { register, handleSubmit, formState: { errors } } = useForm<CreatePostForm>({
+      resolver: zodResolver(CreatePostSchema),
+  });
 
   const createPostMutation = useMutation({
-      mutationFn: () => createPost(text),
+      mutationFn: createPost,
       onSuccess: () => {
           queryClient.invalidateQueries({ queryKey: ["posts"] });
       }
   }, queryClient);
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-      event.preventDefault();
-      createPostMutation.mutate();
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="post-form">
-      <FormField label="Текст поста">
+    <form onSubmit={handleSubmit(({ text }) => createPostMutation.mutate(text))} className="post-form">
+      <FormField label="Текст поста" errorMessage={errors.text?.message}>
         <textarea
             className="post-form__input"
-            value={text}
-            onChange={(e) => setText(e.currentTarget.value)}
+            {...register("text")}
         />
       </FormField>
-
       <Button type="submit" title="Опубликовать" isLoading={createPostMutation.isPending} />
     </form>
   );
